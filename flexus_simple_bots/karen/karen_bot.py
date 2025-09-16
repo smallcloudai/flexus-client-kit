fec217240dbfef34dfc70334544e812e5462aaef
Kirill Starkov <starkov.kirill123@gmail.com>
2025-09-16 23:06:35 +0300

    karen 0.1.15

--- flexus_simple_bots/karen/karen_bot.py
+++ flexus_simple_bots/karen/karen_bot.py
@@ -18,7 +18,7 @@ logger = logging.getLogger("bot_karen")
 
 
 BOT_NAME = "karen"
-BOT_VERSION = "0.1.14"
+BOT_VERSION = "0.1.15"
 BOT_VERSION_INT = ckit_client.marketplace_version_as_int(BOT_VERSION)
 
 
