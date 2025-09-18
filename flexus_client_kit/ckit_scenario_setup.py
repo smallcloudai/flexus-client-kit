e7369a55cc2698044903e17b535bdde14bf9a1e1
Humberto Yusta <humbertoyusta02@gmail.com>
2025-09-18 12:00:53 +0200

    refactor_move methods inside class

--- flexus_client_kit/ckit_scenario_setup.py
+++ flexus_client_kit/ckit_scenario_setup.py
@@ -41,82 +41,16 @@ async def select_workspace(
     return next((w for w in bs.workspaces if w.have_admin), bs.workspaces[0])
 
 
-async def create_test_group(fclient: ckit_client.FlexusClient, ws: ckit_client.FWorkspaceOutput, prefix: str = "test") -> str:
-    async with (await fclient.use_http()) as http:
-        return (await http.execute(
-            gql.gql("""mutation($input: FlexusGroupInput!){group_create(input:$input){fgroup_id}}"""),
-            variable_values={"input": {"fgroup_name": f"{prefix}-{uuid.uuid4().hex[:6]}", "fgroup_parent_id": ws.ws_root_group_id}},
-        ))["group_create"]["fgroup_id"]
-
-
-async def install_persona(
-    fclient: ckit_client.FlexusClient,
-    bs: ckit_client.BasicStuffOutput,
-    ws: ckit_client.FWorkspaceOutput,
-    fgroup_id: str,
-    persona_name: str,
-    name: Optional[str] = None,
-    setup: Optional[dict] = None,
-    require_dev: bool = False
-) -> ckit_bot_exec.FPersonaOutput:
-    args = {
-        "client": fclient, "ws_id": ws.ws_id, "inside_fgroup": fgroup_id,
-        "persona_marketable_name": persona_name, "new_setup": setup or {},
-        "persona_name": name or f"{persona_name} Test {fgroup_id[-4:]}"
-    }
-
-    try:
-        install = await ckit_bot_install.bot_install_from_marketplace(**args, install_dev_version=True)
-    except Exception as e:
-        if require_dev:
-            raise e
-        install = await ckit_bot_install.bot_install_from_marketplace(**args, install_dev_version=False)
-
-    async with (await fclient.use_http()) as http:
-        persona_details = await http.execute(gql.gql("""
-            query PersonaGet($id: String!) {
-                persona_get(id: $id) {
-                    persona_marketable_version
-                }
-            }"""), variable_values={"id": install.persona_id})
-
-    return ckit_bot_exec.FPersonaOutput(
-        owner_fuser_id=bs.fuser_id, located_fgroup_id=fgroup_id, persona_id=install.persona_id,
-        persona_name=args["persona_name"], persona_marketable_name=persona_name,
-        persona_marketable_version=persona_details["persona_get"]["persona_marketable_version"], persona_discounts=None,
-        persona_setup=dict(setup or {}), persona_created_ts=time.time(),
-        ws_id=ws.ws_id, ws_timezone="UTC"
-    )
-
-
-async def setup_test_files_in_mongo(bot_fclient: ckit_client.FlexusClient, persona_id: str, workdir: str) -> AsyncMongoClient:
-    import os
-    os.makedirs(workdir, exist_ok=True)
-    Image.new('RGB', (100, 100), color='red').save(f'{workdir}/1.png')
-    Image.new('RGB', (100, 100), color='blue').save(f'{workdir}/2.png')
-    open(f'{workdir}/1.txt', 'w').write('This is test file 1\nWith multiple lines\nFor testing file attachments')
-    open(f'{workdir}/2.json', 'w').write('{"test": "data", "file": "2", "content": "json test file"}')
-
-    mongo_collection = AsyncMongoClient(await ckit_mongo.get_mongodb_creds(bot_fclient, persona_id))[persona_id + "_db"]["files"]
-    for filename in ['1.png', '2.png', '1.txt', '2.json']:
-        with open(f'{workdir}/{filename}', 'rb') as f:
-            await ckit_mongo.store_file(mongo_collection, filename, f.read())
-    return mongo_collection
-
-
-async def cleanup_test_group(fclient: ckit_client.FlexusClient, fgroup_id: str) -> None:
-    try:
-        async with (await fclient.use_http()) as http:
-            await http.execute(gql.gql("""mutation($id:String!){group_delete(fgroup_id:$id)}"""), variable_values={"id": fgroup_id})
-    except Exception as e:
-        print(f"⚠️ Failed to delete test group {fgroup_id}: {e}")
-
-
 class ScenarioSetup:
     def __init__(self, service_name: str = "test_scenario"):
         self.fclient = ckit_client.FlexusClient(service_name=service_name)
         self.bot_fclient = ckit_client.FlexusClient(service_name=f"{service_name}_bot", endpoint="/v1/jailed-bot")
         self.fgroup_id: Optional[str] = None
+        self.persona: Optional[ckit_bot_exec.FPersonaOutput] = None
+        self.rcx: Optional[ckit_bot_exec.RobotContext] = None
+        self.mongo_collection: Optional[AsyncMongoClient] = None
+        self.bs: Optional[ckit_client.BasicStuffOutput] = None
+        self.ws: Optional[ckit_client.FWorkspaceOutput] = None
 
     async def setup(
         self,
@@ -125,12 +59,68 @@ class ScenarioSetup:
         require_dev: bool = False,
         prefix: str = "test"
     ) -> tuple[ckit_bot_exec.RobotContext, AsyncMongoClient]:
-        bs = await ckit_client.query_basic_stuff(self.fclient)
-        ws = await select_workspace(self.fclient, bs, persona_name, require_dev)
-        self.fgroup_id = await create_test_group(self.fclient, ws, prefix)
-        persona = await install_persona(self.fclient, bs, ws, self.fgroup_id, persona_name, setup=setup, require_dev=require_dev)
-        rcx = ckit_bot_exec.RobotContext(self.bot_fclient, persona)
-        return rcx, await setup_test_files_in_mongo(self.bot_fclient, persona.persona_id, rcx.workdir)
+        self.bs = await ckit_client.query_basic_stuff(self.fclient)
+        self.ws = await select_workspace(self.fclient, self.bs, persona_name, require_dev)
+        await self.create_test_group(prefix)
+        await self.install_persona(persona_name, setup, require_dev)
+        await self.setup_mongo()
+        return self.rcx, self.mongo_collection
+
+    async def create_test_group(self, prefix: str) -> None:
+        async with (await self.fclient.use_http()) as http:
+            self.fgroup_id = (await http.execute(
+                gql.gql("""mutation($input: FlexusGroupInput!){group_create(input:$input){fgroup_id}}"""),
+                variable_values={"input": {"fgroup_name": f"{prefix}-{uuid.uuid4().hex[:6]}", "fgroup_parent_id": self.ws.ws_root_group_id}},
+            ))["group_create"]["fgroup_id"]
+
+    async def install_persona(
+        self,
+        persona_name: str,
+        setup: Optional[dict] = None,
+        require_dev: bool = False,
+    ) -> None:
+        args = {
+            "client": self.fclient, "ws_id": self.ws.ws_id, "inside_fgroup": self.fgroup_id,
+            "persona_marketable_name": persona_name, "new_setup": setup or {},
+            "persona_name": f"{persona_name} Test {self.fgroup_id[-4:]}"
+        }
+
+        try:
+            install = await ckit_bot_install.bot_install_from_marketplace(**args, install_dev_version=True)
+        except Exception as e:
+            if require_dev:
+                raise e
+            install = await ckit_bot_install.bot_install_from_marketplace(**args, install_dev_version=False)
+
+        async with (await self.fclient.use_http()) as http:
+            persona_details = await http.execute(gql.gql("""
+                query PersonaGet($id: String!) {
+                    persona_get(id: $id) {
+                        persona_marketable_version
+                    }
+                }"""), variable_values={"id": install.persona_id})
+
+        self.persona = ckit_bot_exec.FPersonaOutput(
+            owner_fuser_id=self.bs.fuser_id, located_fgroup_id=self.fgroup_id, persona_id=install.persona_id,
+            persona_name=args["persona_name"], persona_marketable_name=persona_name,
+            persona_marketable_version=persona_details["persona_get"]["persona_marketable_version"], persona_discounts=None,
+            persona_setup=dict(setup or {}), persona_created_ts=time.time(),
+            ws_id=self.ws.ws_id, ws_timezone="UTC"
+        )
+        self.rcx = ckit_bot_exec.RobotContext(self.bot_fclient, self.persona)
+
+    async def setup_mongo(self) -> None:
+        import os
+        os.makedirs(self.rcx.workdir, exist_ok=True)
+        Image.new('RGB', (100, 100), color='red').save(f'{self.rcx.workdir}/1.png')
+        Image.new('RGB', (100, 100), color='blue').save(f'{self.rcx.workdir}/2.png')
+        open(f'{self.rcx.workdir}/1.txt', 'w').write('This is test file 1\nWith multiple lines\nFor testing file attachments')
+        open(f'{self.rcx.workdir}/2.json', 'w').write('{"test": "data", "file": "2", "content": "json test file"}')
+
+        self.mongo_collection = AsyncMongoClient(await ckit_mongo.get_mongodb_creds(self.bot_fclient, self.persona.persona_id))[self.persona.persona_id + "_db"]["files"]
+        for filename in ['1.png', '2.png', '1.txt', '2.json']:
+            with open(f'{self.rcx.workdir}/{filename}', 'rb') as f:
+                await ckit_mongo.store_file(self.mongo_collection, filename, f.read())
 
     async def create_mcp(
         self,
@@ -208,4 +198,8 @@ class ScenarioSetup:
 
     async def cleanup(self) -> None:
         if self.fgroup_id:
-            await cleanup_test_group(self.fclient, self.fgroup_id)
+            try:
+                async with (await self.fclient.use_http()) as http:
+                    await http.execute(gql.gql("""mutation($id:String!){group_delete(fgroup_id:$id)}"""), variable_values={"id": self.fgroup_id})
+            except Exception as e:
+                print(f"⚠️ Failed to delete test group {self.fgroup_id}: {e}")
