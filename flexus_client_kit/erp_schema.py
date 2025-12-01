1a8d9e5552c25370149d774968088bceb88aa9c8
Humberto Yusta <humbertoyusta02@gmail.com>
2025-12-01 16:35:27 +0100

    string ids, relations, and more tables to schema

--- flexus_client_kit/erp_schema.py
+++ flexus_client_kit/erp_schema.py
@@ -5,7 +5,7 @@ from typing import Optional, Dict, Type, List
 
 @dataclass
 class CrmContact:
-    contact_id: int
+    contact_id: str
     ws_id: str
     contact_first_name: str
     contact_last_name: str
@@ -34,23 +34,41 @@ class CrmContact:
 
 
 @dataclass
+class CrmTask:
+    ws_id: str
+    contact_id: str
+    task_type: str
+    task_title: str
+    task_notes: str = ""
+    task_details: dict = field(default_factory=dict)
+    task_id: str = ""
+    task_due_ts: float = 0.0
+    task_completed_ts: float = 0.0
+    task_created_ts: float = field(default_factory=time.time)
+    task_updated_ts: float = field(default_factory=time.time)
+    contact: Optional['CrmContact'] = None
+
+
+@dataclass
 class ProductTemplate:
-    prodt_id: int
+    prodt_id: str
     prodt_name: str
     prodt_type: str
-    prodt_pcat_id: int
+    prodt_pcat_id: str
     prodt_list_price: int  # stored in cents
     prodt_standard_price: int  # stored in cents
-    prodt_uom_id: int
+    prodt_uom_id: str
     prodt_active: bool
     ws_id: str
     prodt_chips: List[str]
+    pcat: Optional['ProductCategory'] = None
+    uom: Optional['ProductUom'] = None
 
 
 @dataclass
 class ProductProduct:
-    prod_id: int
-    prodt_id: int
+    prod_id: str
+    prodt_id: str
     prod_default_code: Optional[str]
     prod_barcode: Optional[str]
     prod_active: bool
@@ -59,24 +77,53 @@ class ProductProduct:
 
 
 @dataclass
-class CrmTask:
+class ProductCategory:
+    pcat_id: str
+    pcat_name: str
+    pcat_parent_id: Optional[str]
+    pcat_active: bool
     ws_id: str
-    contact_id: int
-    task_type: str
-    task_title: str
-    task_notes: str = ""
-    task_details: dict = field(default_factory=dict)
-    task_id: int = 0
-    task_due_ts: float = 0.0
-    task_completed_ts: float = 0.0
-    task_created_ts: float = field(default_factory=time.time)
-    task_updated_ts: float = field(default_factory=time.time)
+    parent: Optional['ProductCategory'] = None
+
+
+@dataclass
+class ProductTag:
+    tag_id: str
+    tag_name: str
+    tag_sequence: int
+    tag_color: str
+    tag_visible_to_customers: bool
+    ws_id: str
+
+
+@dataclass
+class ProductUom:
+    uom_id: str
+    uom_name: str
+    uom_category_id: Optional[str]
+    uom_active: bool
+    ws_id: str
+    category: Optional[ProductCategory] = None
+
+
+@dataclass
+class ProductM2mTemplateTag:
+    id: str
+    tag_id: str
+    prodt_id: str
+    ws_id: str
+    tag: Optional[ProductTag] = None
+    prodt: Optional['ProductTemplate'] = None
 
 
 ERP_TABLE_TO_SCHEMA: Dict[str, Type] = {
-    "product_product": ProductProduct,
-    "product_template": ProductTemplate,
     "crm_contact": CrmContact,
     "crm_task": CrmTask,
+    "product_template": ProductTemplate,
+    "product_product": ProductProduct,
+    "product_category": ProductCategory,
+    "product_tag": ProductTag,
+    "product_uom": ProductUom,
+    "product_m2m_template_tag": ProductM2mTemplateTag,
 }
 
