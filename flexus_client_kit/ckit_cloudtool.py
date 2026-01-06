8883c58ed50e6e6876e8504d809a53b41e82fcae
Hamza Hasan Ellahie <hamzahasanellahie000@gmail.com>
2026-01-06 19:00:03 +0500

    post "result"

--- flexus_client_kit/ckit_cloudtool.py
+++ flexus_client_kit/ckit_cloudtool.py
@@ -209,8 +209,8 @@ async def call_python_function_and_save_result(
     except Exception as e:
         logger.warning("error processing call %s %s:%03d:%03d %+d: %s %s" % (call.fcall_id, call.fcall_ft_id, call.fcall_ftm_alt, call.fcall_called_ftm_num, call.fcall_call_n, type(e).__name__, e), exc_info=e)
         content, prov = json.dumps(f"{type(e).__name__} {e}"), json.dumps({"system": service_name})
-    if content is not None:
-        await cloudtool_post_result(fclient, call.fcall_id, call.fcall_untrusted_key, content, prov, dollars)
+    if result is not None:
+        await cloudtool_post_result(fclient, call.fcall_id, call.fcall_untrusted_key, result, prov, dollars)
 
 
 async def cloudtool_post_result(fclient: ckit_client.FlexusClient, fcall_id: str, fcall_untrusted_key: str, content: str, prov: str, dollars: float = 0.0, as_placeholder: bool = False):
