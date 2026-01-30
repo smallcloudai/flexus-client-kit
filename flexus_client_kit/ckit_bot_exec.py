9b1082e05c99b929791434dcaa9058a6fb2ce620
Oleg Klimov <omgtech@gmail.com>
2026-01-30 17:55:01 +0100

    minor

--- flexus_client_kit/ckit_bot_exec.py
+++ flexus_client_kit/ckit_bot_exec.py
@@ -256,7 +256,7 @@ class RobotContext:
             serialized_result = json.dumps("WAIT_SUBCHATS")
             subchats_list = e.subchats
         except ckit_cloudtool.AlreadyPostedResult:
-            logger.info("call %s result already posted" % toolcall.fcall_id)
+            logger.info("call %s result already posted (or faked for scenario)" % toolcall.fcall_id)
             return
         except ckit_cloudtool.NeedsConfirmation as e:
             logger.info("%s needs human confirmation: %s" % (toolcall.fcall_id, e.confirm_explanation))
