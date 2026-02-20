fcffb2706583d8a18572de0859a8d77d49201f96
Oleg Klimov <omgtech@gmail.com>
2026-02-20 05:43:38 +0100

    fixing all_install.sh

--- all_install.sh
+++ all_install.sh
@@ -3,12 +3,12 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <workspace_id>"
     exit 1
 fi
-WS_ID=$1
-python -m flexus_simple_bots.frog.frog_install --ws "$WS_ID"
-python -m flexus_simple_bots.slonik.slonik_install --ws "$WS_ID"
-python -m flexus_simple_bots.karen.karen_install --ws "$WS_ID"
-python -m flexus_simple_bots.boss.boss_install --ws "$WS_ID"
-python -m flexus_simple_bots.admonster.admonster_install --ws "$WS_ID"
-python -m flexus_simple_bots.productman.productman_install --ws "$WS_ID"
-python -m flexus_simple_bots.telegram_groupmod.telegram_groupmod_install --ws "$WS_ID"
+export FLEXUS_WORKSPACE=$1
+python -m flexus_simple_bots.frog.frog_install
+python -m flexus_simple_bots.slonik.slonik_install
+python -m flexus_simple_bots.karen.karen_install
+python -m flexus_simple_bots.boss.boss_install
+python -m flexus_simple_bots.admonster.admonster_install
+python -m flexus_simple_bots.productman.productman_install
+python -m flexus_simple_bots.telegram_groupmod.telegram_groupmod_install
 
