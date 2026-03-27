185289a0664e7b1a89b302a0943d263fb7d16be6
Humberto Yusta <humbertoyusta02@gmail.com>
2026-03-27 22:28:33 +0100

    vix can verify email while talking to messenger platform

--- flexus_simple_bots/vix/vix_bot.py
+++ flexus_simple_bots/vix/vix_bot.py
@@ -46,7 +46,7 @@ VIX_INTEGRATIONS: list[ckit_integrations_db.IntegrationRecord] = ckit_integratio
         "flexus_policy_document",
         "print_widget",
         "erp[meta, data, crud, csv_import]",
-        "crm[manage_contact, manage_deal, log_activity]",
+        "crm[manage_contact, manage_deal, log_activity, verify_email]",
         "magic_desk",
         "slack",
         "telegram",
