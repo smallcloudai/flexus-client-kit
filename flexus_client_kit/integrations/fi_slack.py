e4703a2c87df3096e949f496242bf53cbe1f1b02
Humberto Yusta <humbertoyusta02@gmail.com>
2026-04-06 13:15:33 +0200

    prefer thread app specific, to prevent setup update resending messages

--- flexus_client_kit/integrations/fi_slack.py
+++ flexus_client_kit/integrations/fi_slack.py
@@ -590,11 +590,9 @@ class IntegrationSlack(fi_messenger.FlexusMessenger):
             return False
         something_id, thread_ts = match.groups()
 
-        app_specific = msg.ft_app_specific
-        if app_specific is None:
-            t = self.rcx.latest_threads.get(msg.ftm_belongs_to_ft_id)
-            if t:
-                app_specific = t.thread_fields.ft_app_specific
+        # msg-level ft_app_specific is stale; thread-level has the latest last_posted_assistant_ts
+        t = self.rcx.latest_threads.get(msg.ftm_belongs_to_ft_id)
+        app_specific = (t.thread_fields.ft_app_specific if t else None) or msg.ft_app_specific
         last_posted_ts = (app_specific or {}).get("last_posted_assistant_ts")
         if last_posted_ts is None:
             logger.warning("slack dedup: no last_posted_assistant_ts ft=%s ft_app_specific=%r", msg.ftm_belongs_to_ft_id, app_specific)
